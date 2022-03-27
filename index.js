9f72adf8682fe0407a5cbaa9c359937cbe128dd3
Jeremy <jplatt4427@gmail.com>
2022-03-26 17:25:16 -0700

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "Tom";
+
+
+function upperCaseCustomerName(){
+    customerName = "BOB";
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "definitly tom";
+}
